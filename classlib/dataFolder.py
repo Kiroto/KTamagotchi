adeb7dc3a95c4b430b29bd13e0f1301f6f6d382a
Jerry Rivas <kiroto50@gmail.com>
2022-05-19 13:49:04 -0400

    refactored data folder instructions elsewhere

---
+++ classlib/dataFolder.py
@@ -0,0 +1,33 @@
+from classlib.file import File
+
+
+import os
+
+from classlib.pet import Pet
+
+class GameDataFolder():
+    # Obtains the Game Data folder
+    @staticmethod
+    def getGameDataFolder():
+        appData = os.getenv("APPDATA")
+        return File(os.path.join(appData, "KTamagotchi"))
+
+    def __initializeAppData(self):
+        self.file.mkDir()
+
+    def __init__(self):
+        self.file = GameDataFolder.getGameDataFolder()
+        self.__initializeAppData()
+
+    def savePet(self, pet: Pet):
+        petFile = self.file.resolve("pet.ktg")
+        if not petFile.isFile():
+            petFile.mkFile()
+        petFile.pickleDump(pet)
+
+    def loadPet(self):
+        petFile = self.file.resolve("pet")
+        if not petFile.isFile():
+            raise "No Pet File"
+        loadedPet: Pet = petFile.pickleLoad()
+        return loadedPet
--- game.py
+++ game.py
@@ -1,36 +1,9 @@
-import os
 from time import sleep
-from classlib.file import File
+from classlib.dataFolder import GameDataFolder
 from classlib.pet import Pet
 from classlib.utils.colorlib import RGBColor, printColorString, resetColor
 
-
-def getGameDataFolder():
-    appData = os.getenv("APPDATA")
-    return File(os.path.join(appData, "KTamagotchi"))
-
-
-dataFolder = getGameDataFolder()
-
-
-def initializeAppData():
-    dataFolder.mkDir()
-
-
-def savePet(pet: Pet):
-    petFile = dataFolder.resolve("pet")
-    if not petFile.isFile():
-        petFile.mkFile()
-    petFile.pickleDump(pet)
-
-
-def loadPet():
-    petFile = dataFolder.resolve("pet")
-    if not petFile.isFile():
-        raise "No Pet File"
-    loadedPet: Pet = petFile.pickleLoad()
-    return loadedPet
-
+gameDataFolder = GameDataFolder()
 
 def end():
     printColorString("GoodBye")
@@ -38,12 +11,11 @@ def end():
 
 
 def main():
-    initializeAppData()
     pet = Pet()
     pet.name = "Bakkelito♦♣♠"
-    savePet(pet)
+    gameDataFolder.savePet(pet)
     pet.name = "Romeo"
-    pet = loadPet()
+    pet = gameDataFolder.loadPet()
     printColorString(pet.name, RGBColor(0xaaaaff))
     printColorString(pet.health, RGBColor(0x44ee66), RGBColor(0x111155))
     end()
